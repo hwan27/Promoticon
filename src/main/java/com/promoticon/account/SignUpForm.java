8a095cf4bdbd3c8596353edf2c7746bc39b8677b
Hwan <meadea27@gmail.com>
2022-04-09 17:10:45 +0900

    initial and signup

---
+++ src/main/java/com/promoticon/account/SignUpForm.java
@@ -0,0 +1,13 @@
+package com.promoticon.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String username;
+
+    private String email;
+
+    private String password;
+}
---
+++ src/test/java/com/promoticon/PromoticonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promoticon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoticonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
