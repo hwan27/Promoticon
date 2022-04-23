e5958d1da0b2c9596769adaf492b7efec76df820
Hwan <meadea27@gmail.com>
2022-04-24 03:55:17 +0900

    profile form

--- src/main/java/com/promoticon/account/AccountController.java
+++ src/main/java/com/promoticon/account/AccountController.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -78,11 +75,22 @@ public class AccountController {
             return view;
         }
 
-        account.completeSignUp();
-        accountService.login(account);
+        accountService.completeSignUp(account);
 
         model.addAttribute("numberOfUser", accountRepository.count());
         model.addAttribute("username", account.getUsername());
         return view;
+    }
+
+    @GetMapping("/profile/{username}")
+    public String viewProfile(@PathVariable String username, Model model, @CurrentUser Account account) {
+        Account byUsername = accountRepository.findByUsername(username);
+        if (byUsername == null) {
+            throw new IllegalArgumentException(username + "에 해당하는 사용자가 없습니다.");
+        }
+
+        model.addAttribute("account", byUsername);
+        model.addAttribute("isOwner", byUsername.equals(account));
+        return "account/profile";
     }
 }
--- src/main/java/com/promoticon/account/AccountService.java
+++ src/main/java/com/promoticon/account/AccountService.java
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
 
@@ -25,7 +26,6 @@ public class AccountService implements UserDetailsService {
     private final JavaMailSender javaMailSender;
     private final PasswordEncoder passwordEncoder;
 
-    @Transactional
     public Account processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccount(signUpForm);
         newAccount.generateEmailCheckToken();
@@ -65,6 +65,7 @@ public class AccountService implements UserDetailsService {
         SecurityContextHolder.getContext().setAuthentication(token);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public UserDetails loadUserByUsername(String emailOrUsername) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(emailOrUsername);
@@ -77,5 +78,11 @@ public class AccountService implements UserDetailsService {
         }
 
         return new UserAccount(account);
+    }
+
+    public void completeSignUp(Account account) {
+
+        account.completeSignUp();
+        login(account);
     }
 }
